9137fd89f8756050aa602756aba20704f7e8ee9e
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2025-02-27 14:03:07 +0300

    fix

--- src/components/ConnectWalletButton.jsx
+++ src/components/ConnectWalletButton.jsx
@@ -6,44 +6,46 @@ import WarningLogo from '../assets/images/warning-icon.svg';
 import { ChevronBarDown, ChevronDown, Wallet2 } from 'react-bootstrap-icons';
 import { readContract } from '@wagmi/core'
 import { monadTestnet } from 'viem/chains';
-
 import { keccak256, namehash } from 'viem'
+import { useState } from 'react';
+import { wagmiAdapter } from "../config";
 
-
-
+ 
 export default function ConnectWalletButton({props}) {
   const { open } = useAppKit()
   const { address, isConnected  } = useAccount() 
   const { switchChain } = useSwitchChain() 
   const chainId = useChainId()
+  const [name, setName] = useState(null);
   const SUPPORTED_CHAIN_ID = Number(import.meta.env.VITE_APP_SUPPORTED_CHAIN_ID);
 
-   
-  if(isConnected) { 
-
-     
-
+  async function reverseLookkup(addr) {
     const abi = [
       {
         type: "function",
         name: "name",
         stateMutability: "view",
         inputs: [{ name: "node", type: "bytes32" }],
-        outputs: [{ type: "string" }],
+        outputs: [{ type: "string" }]
       }, 
     ]
  
-    const publicResolverConfig = {
-        address: import.meta.env.VITE_APP_PUBLIC_RESOLVER,
-        abi: abi
-    };
-  
-    const { data: name, error } = useReadContract({
-        ...publicResolverConfig,
+    
+    const result = await readContract(wagmiAdapter.wagmiConfig, {
+        abi,
         functionName: 'name',
-        args: [getNameHash( address.slice(2) +".addr.reverse")],
+        address: import.meta.env.VITE_APP_PUBLIC_RESOLVER,
+        args: [getNameHash( addr.slice(2) +".addr.reverse")],
         chainId: import.meta.env.VITE_APP_NODE_ENV === "production" ? monadTestnet.id: monadTestnet.id
     });
+
+    if(result) setName(result);
+  }
+
+   
+  if(isConnected) { 
+    
+    reverseLookkup(address);
     
     return (<>  { SUPPORTED_CHAIN_ID !== chainId ?
         <button {...props} className="btn btn-danger fs-5 border-0" onClick={() => switchChain({ chainId: SUPPORTED_CHAIN_ID })}> Wrong Network <img src={WarningLogo} /></button>  
