a59db139ab8a8c3fbd1fdd1cd89f05949fd70af9
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2025-04-21 13:20:31 +0300

    render fix

--- server/src/core/server.ts
+++ server/src/core/server.ts
@@ -17,25 +17,26 @@ export class Server {
 
     async start() : Promise<void> { 
 
+        this.app.use(Express.json())
+        this.app.use(Express.urlencoded({ extended: true }))
+ 
+        this.app.set("trust proxy", true);
+        this.app.set('view engine', 'html');
+        this.app.set('views', 'dist');
+        this.app.engine('html', ejs.renderFile);
+        ejs.delimiter = '?';
+         
         this.app.locals.name = null;
         this.app.locals.timestamp = null;
         this.app.locals.canonicalUrl = "";
         this.app.locals.title = null;
         this.app.locals.description = null;
 
-        this.app.use(Express.json())
-        this.app.use(Express.urlencoded({ extended: true }))
-
-        ejs.delimiter = '?';
-    
-        this.app.set("trust proxy", true);
-        this.app.engine('html', ejs.renderFile);
-        this.app.set('view engine', 'html');
-        this.app.set('views', 'dist');
- 
         this.app.use("/", (req: Request, res: Response, next: NextFunction ) => {
-            if(req.url != "/") return next();
-            res.render("index", { ogImageUrl: process.env.OG_DEFAULT_IMAGE_URL })
+            //if(req.url != "/") return next();
+            res.render("index", {
+                timestamp: Date.now() 
+            })
         }); 
 
         this.app.use(["/name/:name.mon", "/name/:name"], (req: Request, res: Response, next: NextFunction ) => {
@@ -53,17 +54,26 @@ export class Server {
         }); 
 
         this.app.use(["/account", "/register/:name"], (req: Request, res: Response, next: NextFunction ) => {
-            res.render("index", { timestamp: Date.now() })
+            res.render("index", {  
+                timestamp: Date.now() 
+            })
         });  
 
         this.app.use(Express.static('dist'));
  
         this.app.use((req: Request, res: Response, next: NextFunction ) => {
-            res.status(HttpCode.NOT_FOUND).render("index");
+            res.status(HttpCode.NOT_FOUND).render("index", {
+                title: `Page Not Found`,
+                timestamp: Date.now() 
+            });
         })
 
         this.app.use((error: Error, req: Request, res: Response, next: NextFunction ) => {
-            res.status(HttpCode.INTERNAL_SERVER_ERROR).render("index");
+            res.status(HttpCode.INTERNAL_SERVER_ERROR).render("index", { 
+                title: `Error`,
+                description: `${ error.message }`,
+                timestamp: Date.now() 
+             });
         })
  
         this.app.listen(this.port, ()=> {
