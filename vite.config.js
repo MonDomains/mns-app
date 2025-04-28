1a6049f10528ab866c58f659a95dcc32fed632db
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2025-04-28 14:41:30 +0300

    cloudflare vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve, dirname } from 'node:path'
-import { fileURLToPath, URL } from 'node:url'
+import react from '@vitejs/plugin-react' 
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), cloudflare()]
 })
